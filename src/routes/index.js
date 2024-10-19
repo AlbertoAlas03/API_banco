2ed177a5dc9bb4a4b88ad78c9df626e4158ceeb4
Alberto Alas <69991549+AlbertoAlas03@users.noreply.github.com>
2024-10-19 09:52:54 -0600

    Adción de ruta para rechazar/acpetar solicitud

--- src/routes/index.js
+++ src/routes/index.js
@@ -32,5 +32,6 @@ router.delete('/api/deleteegreso/:id', dataController.deleteegreso)
 router.get('/api/listusuario', dataController.listusuario)
 
 router.post('/api/addusuario', dataController.addusuario)
+router.post('/api/modifyUsuario/:id', dataController.modifyUsuario)
 
 module.exports = router;
